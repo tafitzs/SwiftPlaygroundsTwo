b409dc88c1dbc2dbbaa743782d67b7ebe9cf4e2e
tafitzs <tafitzs@hawaii.edu>
2018-01-29 10:39:34 -1000

    completed SW 2

--- PlaygroundTwo.playground/Contents.swift
+++ PlaygroundTwo.playground/Contents.swift
@@ -3,3 +3,27 @@
 import UIKit
 
 var str = "Hello, playground"
+
+var goodGrade = "I've been doing all my work"
+if goodGrade == "I've been doing all my work"
+{
+    print ("I should be getting an A at this point")
+}
+
+//Evaluate the Condition To Be False
+
+//Part 3, #2
+if goodGrade == "I'm not doing very well"
+{
+print("I'm doing well in this class")
+}
+
+//part 4
+if goodGrade == "I've been doing all my work"
+{
+    print("I'm doing well in this class")
+}
+else
+{
+    print("I'm not doing too well")
+}
